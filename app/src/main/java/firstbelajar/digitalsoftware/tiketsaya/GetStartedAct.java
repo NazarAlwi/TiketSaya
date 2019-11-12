725c6f5cff9e398ff5f432a0d61d94e07ff13c60
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-12 16:49:16 +0700

    Add animation in get strated

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/GetStartedAct.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/GetStartedAct.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStartedAct extends AppCompatActivity {
 
     Button button_sign_in, btn_new_account_on_getStarted;
+    Animation topToBottom, bottomToTop;
+    ImageView emblemApp;
+    TextView introApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
+        topToBottom = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
+        bottomToTop = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top);
+
+        emblemApp = findViewById(R.id.emblem_app);
+        introApp = findViewById(R.id.intro_app);
         button_sign_in = findViewById(R.id.button_sign_in);
         btn_new_account_on_getStarted= findViewById(R.id.btn_new_account_on_getStarted);
+
+        emblemApp.setAnimation(topToBottom);
+        introApp.setAnimation(topToBottom);
+        button_sign_in.setAnimation(bottomToTop);
+        btn_new_account_on_getStarted.setAnimation(bottomToTop);
+
         button_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
