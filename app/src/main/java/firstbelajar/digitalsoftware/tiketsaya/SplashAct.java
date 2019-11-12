6d0d2c0917772f68c613b1af002628f6e09559b3
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-12 14:38:14 +0700

    Add animation in splash screen

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/SplashAct.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/SplashAct.java
@@ -1,17 +1,32 @@
 package firstbelajar.digitalsoftware.tiketsaya;
 
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashAct extends AppCompatActivity {
+    Animation appSplash, bottomToTop;
+    ImageView appLogo;
+    TextView appSubtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        appSplash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        bottomToTop = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top);
+        appLogo = findViewById(R.id.app_logo);
+        appSubtitle = findViewById(R.id.app_subtitle);
+
+        appLogo.setAnimation(appSplash);
+        appSubtitle.setAnimation(bottomToTop);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
