abca9f1705ecbe2fabf5d60606d5af9f47584c1f
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-14 10:30:00 +0700

    Add fuction for adding user data to database

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/RegisterOneAct.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/RegisterOneAct.java
@@ -1,16 +1,30 @@
 package firstbelajar.digitalsoftware.tiketsaya;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneAct extends AppCompatActivity {
 
     LinearLayout btn_back;
     Button btn_continue;
+    EditText edtUsername, edtPassword, edtEmailAddress;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,10 @@ public class RegisterOneAct extends AppCompatActivity {
 
         btn_back = findViewById(R.id.btn_back);
         btn_continue = findViewById(R.id.btn_continue);
+        edtUsername = findViewById(R.id.edt_username);
+        edtPassword = findViewById(R.id.edt_password);
+        edtEmailAddress = findViewById(R.id.edt_email_address);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +47,27 @@ public class RegisterOneAct extends AppCompatActivity {
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, edtUsername.getText().toString());
+                editor.apply();
+
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(edtUsername.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().child("username").setValue(edtUsername.getText().toString());
+                        dataSnapshot.getRef().child("password").setValue(edtPassword.getText().toString());
+                        dataSnapshot.getRef().child("email_address").setValue(edtEmailAddress.getText().toString());
+                        dataSnapshot.getRef().child("balance").setValue(800);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
                 Intent goToRegisterTwo = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
                 startActivity(goToRegisterTwo);
             }
