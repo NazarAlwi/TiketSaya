b73f60e3f2275ae34b35f36c38a6231d77a69329
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-21 17:34:14 +0700

    Create MyProfileTicketDetailActivity

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/MyProfileAct.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/MyProfileAct.java
@@ -1,13 +1,27 @@
 package firstbelajar.digitalsoftware.tiketsaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
+    private LinearLayout myTicket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        myTicket = findViewById(R.id.my_ticket);
+
+        myTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMyTicketDetail = new Intent(MyProfileAct.this, MyProfileTicketDetailAct.class);
+                startActivity(goToMyTicketDetail);
+            }
+        });
     }
 }
