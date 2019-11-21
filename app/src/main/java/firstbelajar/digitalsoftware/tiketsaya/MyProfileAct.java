b970de32c38c38682cbf7b8c7699404d149bd7c5
M Nazar Alwi <alwinazar75@gmail.com>
2019-11-21 17:53:16 +0700

    Create EditProfileActivity

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/MyProfileAct.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/MyProfileAct.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
     private LinearLayout myTicket;
+    private Button btnEditProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,21 @@ public class MyProfileAct extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
         myTicket = findViewById(R.id.my_ticket);
+        btnEditProfile = findViewById(R.id.btn_edit_profile);
 
         myTicket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goToMyTicketDetail = new Intent(MyProfileAct.this, MyProfileTicketDetailAct.class);
                 startActivity(goToMyTicketDetail);
+            }
+        });
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToEditProfile = new Intent(MyProfileAct.this, EditProfileAct.class);
+                startActivity(goToEditProfile);
             }
         });
     }
