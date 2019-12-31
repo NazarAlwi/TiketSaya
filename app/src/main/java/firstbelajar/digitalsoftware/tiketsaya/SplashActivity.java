b4edd0eb0cc7c55748a4666f1e982285e81c34d4
M Nazar Alwi <alwinazar75@gmail.com>
2019-12-31 19:18:25 +0700

    Add function intent to home if user already login

--- app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/SplashActivity.java
+++ app/src/main/java/firstbelajar/digitalsoftware/tiketsaya/SplashActivity.java
@@ -1,6 +1,7 @@
 package firstbelajar.digitalsoftware.tiketsaya;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,10 @@ public class SplashActivity extends AppCompatActivity {
     Animation appSplash, bottomToTop;
     ImageView appLogo;
     TextView appSubtitle;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
+    String username_key_new = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +32,35 @@ public class SplashActivity extends AppCompatActivity {
         appLogo.startAnimation(appSplash);
         appSubtitle.startAnimation(bottomToTop);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // merubah activity ke activity lain
-                Intent goGetStarted = new Intent(SplashActivity.this, GetStartedActivity.class);
-                startActivity(goGetStarted);
-                finish();
-            }
-        }, 2000);
+        getUsernameLocal();
+    }
+
+    private void getUsernameLocal() {
+        SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+        username_key_new = sharedPreferences.getString(username_key, "");
+
+        if (username_key_new.isEmpty()) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // merubah activity ke activity lain
+                    Intent goGetStarted = new Intent(SplashActivity.this, GetStartedActivity.class);
+                    startActivity(goGetStarted);
+                    finish();
+                }
+            }, 2000);
+        } else {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // merubah activity ke activity lain
+                    Intent goGetHome = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(goGetHome);
+                    finish();
+                }
+            }, 2000);
+        }
     }
 }
